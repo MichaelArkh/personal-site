626ce0465d2abc725def7c22c98f94b2c4685fa9
MichaelArkh <coolmichael26@gmail.com>
2022-03-12 23:15:37 -0500

    Update imports

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Experience from './components/pages/Experience.js'
 import Projects from './components/pages/Projects.js'
 import Contact from './components/pages/Contact.js'
 import ReactGA from "react-ga4";
-import env from "react-dotenv";
 
 ReactGA.initialize(window.env.G_KEY);
 ReactGA.send("pageview");
